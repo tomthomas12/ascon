308fc78cfd9c4a257d8c3cb0841c6102e5fc647b
Tom thomas <33503920+tomthomas12@users.noreply.github.com>
2023-12-14 23:31:40 +0000

    Update dascon.py

--- dascon.py
+++ dascon.py
@@ -1,4 +1,5 @@
 import ascon
+import json
 
 def decrypt_file(encrypted_file, output_file, key):
     with open(encrypted_file, 'rb') as f:
@@ -8,14 +9,32 @@ def decrypt_file(encrypted_file, output_file, key):
     # Optionally provide associated data
     associated_data = b"Associated Data"
 
-    # Perform decryption
-    plaintext = ascon.decrypt(key, nonce, associated_data, ciphertext)
+    try:
+        # Perform decryption
+        plaintext = ascon.decrypt(key, nonce, associated_data, ciphertext)
 
-    with open(output_file, 'wb') as f:
-        f.write(plaintext)
+        with open(output_file, 'wb') as f:
+            f.write(plaintext)
+
+        print("Decryption successful.")
+    except Exception as e:
+        print(f"Decryption failed: {e}")
+
+def load_key_from_json(filename):
+    with open(filename, 'r') as f:
+        key_data = json.load(f)
+
+    key_hex = key_data['key']
+    key_bytes = bytes.fromhex(key_hex)
+
+    return key_bytes
 
 if __name__ == "__main__":
-    key = b'Sixteen byte key'
+    # Read key from JSON file
+    private_key_filename = 'private_key.json'
+    key = load_key_from_json(private_key_filename)
+
+    # Decryption
     encrypted_file = 'encrypted_file.bin'
     decrypted_file = 'decrypted_file.txt'
 
